17d10b8915947149c93d577288c32606af57b07e
leilakf <104149967+leilakf@users.noreply.github.com>
2022-05-16 13:05:59 +0330

    Initial commit

---
+++ weatherr.js
@@ -0,0 +1,68 @@
+
+
+// //day and time
+
+let now = new Date();
+
+let date=document.querySelector(".Day-first");
+let hours=now.getHours();
+let minutes=now.getMinutes();
+
+let days=[
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  
+]
+
+let day=days[now.getDay()];
+
+
+date.innerHTML=`${day} ${hours}:${minutes} `;
+
+
+
+// temprature weather//
+
+//update city
+
+function searchcity (event){
+
+event.preventDefault();
+ let units = "metric";
+let keyapi="d045ef62d06fb179edb328171922730c";
+let city=document.querySelector(".form-control").value;
+let urlapi=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyapi}&units=${units}`;
+
+axios.get(`${urlapi}`).then(showtemp);
+}
+
+  
+ let form=document.querySelector("#search-form");
+ form.addEventListener("submit",searchcity);
+
+function showtemp(response){
+console.log(response);
+  let temperature=Math.round(response.data.main.temp);
+  let span=document.querySelector(".temperature");
+  span.innerHTML=`${temperature}`;
+  let cityname=document.querySelector(".citty");
+  cityname.innerHTML=`${response.data.name}`;
+  let discription=document.querySelector(".see");
+  discription.innerHTML=`${response.data.weather[0].main }`;
+  let windspeed=document.querySelector("#wind");
+  let cityspeed=Math.round(response.data.wind.speed);
+  windspeed.innerHTML=`Wind:${cityspeed} km.h`;
+  let humidity=document.querySelector("#humidity");
+  let cityhumidity=response.data.main.humidity;
+  humidity.innerHTML=`Humidity:${cityhumidity}%`;
+
+}
+
+
+
+  
