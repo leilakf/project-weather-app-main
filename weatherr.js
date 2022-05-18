dbaa265817e5a17573f82b20ddd842225cb2fb60
leilakf <104149967+leilakf@users.noreply.github.com>
2022-05-18 14:20:44 +0330

    update search engine and city

--- weatherr.js
+++ weatherr.js
@@ -30,21 +30,6 @@ date.innerHTML=`${day} ${hours}:${minutes} `;
 
 //update city
 
-function searchcity (event){
-event.preventDefault();
- let units = "metric";
-let keyapi="d045ef62d06fb179edb328171922730c";
-let city=document.querySelector(".form-control").value;
-let urlapi=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyapi}&units=${units}`;
-console.log(urlapi);
-
-axios.get(`${urlapi}`).then(showtemp);
-}
-
-  
- let form=document.querySelector("#search-form");
- form.addEventListener("submit",searchcity);
-
 function showtemp(response){
   let temperature=Math.round(response.data.main.temp);
   let span=document.querySelector(".temperature");
@@ -59,19 +44,60 @@ function showtemp(response){
   let humidity=document.querySelector("#humidity");
   let cityhumidity=response.data.main.humidity;
   humidity.innerHTML=`Humidity:${cityhumidity}%`;
-  // let iconElement=document.querySelector("#icon");
-  // iconElement.setAttribute("src",`icons/${response.data.weather[0].icon}.jpg`);
-
-}
-// city serch under serch city
-function handelsubmit(event){
-    event.preventDefault();
  
-  cityname.innerHTML="#btn1";
 
 }
+// Weather Search / Search engine
 
-let cityElement=document.querySelector("#btn1");
-cityElement.addEventListener("submit",handelsubmit);
+function search (city){
+ let units = "metric";
+let keyapi="d045ef62d06fb179edb328171922730c";
+// let city=document.querySelector(".form-control").value;
+let urlapi=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyapi}&units=${units}`;
+console.log(urlapi);
 
-  
+axios.get(`${urlapi}`).then(showtemp);
+}
+
+
+function handleSubmit(event){
+  event.preventDefault();
+  let cityinputElement=document.querySelector("#city-input");
+    
+  search(cityinputElement.value)
+}
+
+
+let form=document.querySelector("#search-form");
+form.addEventListener("submit",handleSubmit);
+
+search("New York");
+
+
+
+
+
+
+
+
+
+
+// city serch under serch city
+// function handelsubmit(event){
+//     event.preventDefault();
+//  let btn1Element=document.querySelector("#btn1");
+// searchcity(btn1Element);
+
+
+// }
+// function handelsubmit (event){
+//   event.preventDefault();
+//   alert("hi");
+//    let btn1Element=document.querySelector("#btn1");
+
+//   searchcity(btn1Element.value);
+// }
+
+// let cityElement=document.querySelector("#btn1");
+// cityElement.addEventListener("click",handelsubmit);
+/////////////////////////////////////////////////////////
