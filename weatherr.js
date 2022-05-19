0250b6f6c976aa87f8b3d567fc11ae1ff096ea38
leilakf <104149967+leilakf@users.noreply.github.com>
2022-05-19 20:45:43 +0330

    update icon and add api icon

--- weatherr.js
+++ weatherr.js
@@ -31,39 +31,66 @@ date.innerHTML=`${day} ${hours}:${minutes} `;
 //update city
 
 function showtemp(response){
+ 
+  console.log(response.data)
   let temperature=Math.round(response.data.main.temp);
   let span=document.querySelector(".temperature");
-
   span.innerHTML=`${temperature}`;
+
   let cityname=document.querySelector(".citty");
-
   cityname.innerHTML=`${response.data.name}`;
-  let discription=document.querySelector(".see");
 
+  let discription=document.querySelector(".see");
   discription.innerHTML=`${response.data.weather[0].main }`;
+
   let windspeed=document.querySelector("#wind");
   let cityspeed=Math.round(response.data.wind.speed);
-
   windspeed.innerHTML=`Wind:${cityspeed} km.h`;
-  let humidity=document.querySelector("#humidity");
 
+  let humidity=document.querySelector("#humidity");
   let cityhumidity=response.data.main.humidity;
   humidity.innerHTML=`Humidity:${cityhumidity}%`;
 
   CilsiusTemperature=Math.round (response.data.main.temp);
- 
-
+debugger;
+let iconElement= response.data.weather[0].id;
+let mastericon=document.querySelector("#master-icon");
+if (iconElement>=200 && iconElement<=232){
+  mastericon.src="icons/11n.gif";
+}
+if (iconElement>=300 && iconElement<=321){
+  mastericon.src="icons/09d.gif";
+}
+if (iconElement>=500 && iconElement<=531){
+  mastericon.src="icons/09n.gif"; 
+}
+if (iconElement>=600 && iconElement<=622){
+  mastericon.src="icons/13d.gif"; 
+}
+if (iconElement>=701 && iconElement<=781){
+  mastericon.src="icons/50d.gif";
+}
+if (iconElement>=800 && iconElement<801){
+  mastericon.src="icons/04d.gif ";
+}
+if (iconElement>=801 && iconElement<=804){
+  mastericon.src="icons/04n.gif ";
+}
+else{
+  alert("your search is uncorect");
+}
 }
 // Weather Search / Search engine
 
 function search (city){
+  debugger;
  let units = "metric";
 let keyapi="d045ef62d06fb179edb328171922730c";
-// let city=document.querySelector(".form-control").value;
 let urlapi=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyapi}&units=${units}`;
-console.log(urlapi);
+
 
 axios.get(`${urlapi}`).then(showtemp);
+
 }
 
 
@@ -73,8 +100,6 @@ function handleSubmit(event){
     
   search(cityinputElement.value)
 }
-
-
 
 function displayFahrenheitTemperture(event){
 event.preventDefault();
@@ -101,7 +126,7 @@ celsiuslink.classList.add("active");
 fahrenheitlink.classList.remove("active");
 
 }
-
+ 
  
 let CilsiusTemperature=null;
 
@@ -115,30 +140,10 @@ fahrenheitlink.addEventListener("click",displayFahrenheitTemperture);
 let celsiuslink=document.querySelector("#celsius-link");
 celsiuslink.addEventListener("click",displaycelsiusTemperture);
 
-
-search("New York");
-
+search("new york");
 
 
 
 
 
-// city serch under serch city
-// function handelsubmit(event){
-//     event.preventDefault();
-//  let btn1Element=document.querySelector("#btn1");
-// searchcity(btn1Element);
 
-
-// }
-// function handelsubmit (event){
-//   event.preventDefault();
-//   alert("hi");
-//    let btn1Element=document.querySelector("#btn1");
-
-//   searchcity(btn1Element.value);
-// }
-
-// let cityElement=document.querySelector("#btn1");
-// cityElement.addEventListener("click",handelsubmit);
-/////////////////////////////////////////////////////////
